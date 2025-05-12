2f015dba10b48960438abc8f849f9ef045e38f51
Fructz <fructisgfx@gmail.com>
2025-05-12 01:00:14 -0600

    Cambios en DB

--- productos-api/server.js
+++ productos-api/server.js
@@ -15,14 +15,14 @@ const mysql = require('mysql2/promise');
 
 // Configuración de la conexión a la base de datos
 const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'Hola3245', //coloca tu contraseña de tu MySql
-    database: 'sistema_ventas', //Nombramiento de tu Base de datos
-    port: 3306,  //Puerto
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 // =============================================
